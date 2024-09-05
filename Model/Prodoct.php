533793656a4a8571402b1ddd81ba17a77666f4c2
TommasoGalistu <165350030+TommasoGalistu@users.noreply.github.com>
2024-09-05 17:07:22 +0200

    feat: control try catch

--- Model/Prodoct.php
+++ Model/Prodoct.php
@@ -8,7 +8,12 @@ class Prodoct{
     public $descrizione;
 
     function __construct(string $_name, float $_prezzo, string $_img, string $_descrizione){
-        $this->setName($_name);
+        try {
+            $this->setName($_name);
+        } catch (Exception $e) {
+            // se c'è un errore viene stampato in pagina con il valore di exception
+            echo 'Errore: ' . $e->getMessage();
+        }
         $this->prezzo = $_prezzo;
         $this->img = $_img;
         $this->descrizione = $_descrizione;
@@ -16,8 +21,11 @@ class Prodoct{
     }
 
     public function setName($_name){
-        
-        $this->name = $_name;
+        if(strlen($_name) < 3){
+            throw new Exception('Il nome è troppo corto');
+        }else{
+            $this->name = $_name;
+        }
     }
     public function getName(){
         return $this->name;
